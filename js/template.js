c468eae43a6748f78a74951425e89c303ba51ec1
سیدمحمد حسینی <smhosseini@khedesh.ir>
2017-09-15 18:26:56 +0430

    another test

--- js/template.js
+++ js/template.js
@@ -1,32 +1,8 @@
-/* global TrelloPowerUp */
-
-var GRAY_ICON = './images/icon-gray.svg';
-
-var cardButtonCallback = function (t) {
-    console.log("Hello!");
-};
-
 TrelloPowerUp.initialize({
-    'card-buttons': function (t, opts) {
+    'card-buttons': function (t, options) {
         return [{
-            // usually you will provide a callback function to be run on button click
-            // we recommend that you use a popup on click generally
-            icon: GRAY_ICON, // don't use a colored icon here
-            text: 'Open Popup',
-            callback: cardButtonCallback
-        }, {
-            // but of course, you could also just kick off to a url if that's your thing
-            icon: GRAY_ICON,
-            text: 'Just a URL',
-            url: 'https://developers.trello.com',
-            target: 'Trello Developer Site' // optional target for above url
+            icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+            text: 'Estimate Size'
         }];
-    },
-    'show-settings': function (t, options) {
-        return t.popup({
-            title: 'Settings',
-            url: './settings.html',
-            height: 184
-        });
     }
 });

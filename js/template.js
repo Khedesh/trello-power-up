624f4993fc384d31aa1a6599fc000388ae70df09
سیدمحمد حسینی <smhosseini@khedesh.ir>
2017-09-15 15:05:30 +0430

    another chnage

--- js/template.js
+++ js/template.js
@@ -1,63 +1,32 @@
 /* global TrelloPowerUp */
 
-var WHITE_ICON = './images/icon-white.svg';
 var GRAY_ICON = './images/icon-gray.svg';
 
-var parkMap = {
-  acad: 'Acadia National Park',
-  arch: 'Arches National Park',
-  badl: 'Badlands National Park',
-  brca: 'Bryce Canyon National Park',
-  crla: 'Crater Lake National Park',
-  dena: 'Denali National Park',
-  glac: 'Glacier National Park',
-  grca: 'Grand Canyon National Park',
-  grte: 'Grand Teton National Park',
-  olym: 'Olympic National Park',
-  yell: 'Yellowstone National Park',
-  yose: 'Yosemite National Park',
-  zion: 'Zion National Park'
-};
-
-var cardButtonCallback = function(t){
-  var items = Object.keys(parkMap).map(function(parkCode){
-    var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
-    return {
-      text: parkMap[parkCode],
-      url: urlForCode,
-      callback: function(t){
-        return t.attach({ url: urlForCode, name: parkMap[parkCode] })
-        .then(function(){
-          return t.closePopup();
-        })
-      }
-    };
-  });
-
-  return t.popup({
-    title: 'Popup Search Example',
-    items: items,
-    search: {
-      count: 5,
-      placeholder: 'Search National Parks',
-      empty: 'No parks found'
-    }
-  });
+var cardButtonCallback = function (t) {
+    console.log("Hello!");
 };
 
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
-    return [{
-      icon: GRAY_ICON,
-      text: 'Template',
-      callback: cardButtonCallback
-    }];
-  },
-  'show-settings': function(t, options){
-    return t.popup({
-      title: 'Settings',
-      url: './settings.html',
-      height: 184
-    });
-  }
+    'card-buttons': function (t, opts) {
+        return [{
+            // usually you will provide a callback function to be run on button click
+            // we recommend that you use a popup on click generally
+            icon: GRAY_ICON, // don't use a colored icon here
+            text: 'Open Popup',
+            callback: cardButtonCallback
+        }, {
+            // but of course, you could also just kick off to a url if that's your thing
+            icon: GRAY_ICON,
+            text: 'Just a URL',
+            url: 'https://developers.trello.com',
+            target: 'Trello Developer Site' // optional target for above url
+        }];
+    },
+    'show-settings': function (t, options) {
+        return t.popup({
+            title: 'Settings',
+            url: './settings.html',
+            height: 184
+        });
+    }
 });
